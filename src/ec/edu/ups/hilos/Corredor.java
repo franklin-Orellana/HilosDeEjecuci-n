7720ae098078c3ddcb0a7f04c19e4f94a64eb87b
Franklin <Franklin@DESKTOP-OQO8VFM>
2019-07-19 01:26:59 -0500

    creacion matrices y separacion

--- src/ec/edu/ups/hilos/Corredor.java
+++ src/ec/edu/ups/hilos/Corredor.java
@@ -13,39 +13,117 @@ import java.util.Scanner;
  * @author Fernanda
  */
 public class Corredor {
+    /*
+    private String nombre;
+    private int velocidad;
 
+    public Corredor(String nombre, int velocidad) {
+        this.nombre = nombre;
+        this.velocidad = 1000;
+    }
+    /*
+    @Override
+    public void run() {
+        try{
+            System.out.println("Inicia: ");
+            long inicio = System.currentTimeMillis();
+            Thread.sleep(velocidad);
+            long fin = System.currentTimeMillis();
+            System.out.println("Tiempo de llegada: " + this.nombre + " es " + (fin - inicio));
+        }catch(InterruptedException ex){
+            ex.printStackTrace();
+        }
+    }
+    */
     public void Matriz() {
         Random randon = new Random();
         Scanner sn = new Scanner(System.in);
-        System.out.println("Ingrese las columnas");
-        int columnas = sn.nextInt();
- //       System.out.println("Ingrese numero de Filas");
- //       int filas = sn.nextInt();
-        int matriz1[][] = new int[1000][columnas];
-        //int matriz2[][] = new int[filas][columnas];
-        //int resultado[][] = new int[filas][columnas];
-        for (int i = 0; i < matriz1.length; i++) {
-            for (int j = 0; j < matriz1[0].length; j++) {
-                matriz1[i][j] = randon.nextInt(5000);
-          //      matriz2[i][j] = randon.nextInt(5000);
-             //   resultado[i][j] = matriz1[i][j] + matriz2[i][j];
+        System.out.println("Ingrese numero de filas");
+        int f1 = sn.nextInt();
+        System.out.println("Ingrese numero de columnas");
+        int c1 = sn.nextInt();
+
+        int[][] m1 = new int[f1][c1];
+        int[][] m2 = new int[f1][c1];
+
+        int[][] res = new int[f1][c1];
+        int[][] subMat1 = new int[25][25];
+        int[][] subMat2 = new int[50][25];
+        int[][] subMat3 = new int[75][25];
+        int[][] subMat4 = new int[100][25];
+        int cont = -1;
+        int cont1 = -1;
+        int cont2 = -1;
+        int cont3 = -1;
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++) {
+                m1[i][j] = randon.nextInt(10);
+                m2[i][j] = randon.nextInt(10);
+                res[i][j] = (m1[i][j] + m2[i][j]);
             }
         }
+        
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 25; j++) {
+                subMat1[i][j] = res[i][j];
+            }
+        }
+        for (int i = 0; i < 50; i++) {
+       
+            for (int j = 25; j < 50; j++) {
+                cont++;
+                subMat2[i][cont] = res[i][j];
+            }
+            cont=-1;
+        }
+        for (int i = 0; i < 75; i++) {
+  
+            for (int j = 50; j < 75; j++) {
+                cont1++;
+                subMat3[i][cont1] = res[i][j];
+            }
+            cont1=-1;
+        }
+        for (int i = 0; i < 100; i++) {
+
+            for (int j = 75; j < 100; j++) {
+                cont2++;
+                subMat4[i][cont2] = res[i][j];
+            }
+            cont2=-1;
+        }
+
+
         System.out.println("Matriz 1");
-        mostrarMatriz(matriz1);
+        mostrarMatriz(m1);
         System.out.println("Matriz 2");
-      //  mostrarMatriz(matriz2);
-        System.out.println("Resultado");
-        //mostrarMatriz(resultado);
+        mostrarMatriz(m2);
+        System.out.println("Respuesta");
+        mostrarMatriz(res);
+
+        
+        System.out.println("SubMatriz1");
+        mostrarMatriz(subMat1);
+        System.out.println("SubMatriz2");
+        mostrarMatriz(subMat2);
+        System.out.println("SubMatriz3");
+        mostrarMatriz(subMat3);
+        System.out.println("SubMatriz4");
+        mostrarMatriz(subMat4);
+
     }
 
     public static void mostrarMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
+            System.out.print("[");
             for (int j = 0; j < matriz[0].length; j++) {
-                System.out.println(matriz[i][j]+"");
+                System.out.print(matriz[i][j] + "|");
             }
+            System.out.print("]");
+            System.out.println("");
         }
     }
 
-   
+    
+
 }
--- src/ec/edu/ups/principal/Principal.java
+++ src/ec/edu/ups/principal/Principal.java
@@ -7,21 +7,38 @@ package ec.edu.ups.principal;
 
 import java.util.Scanner;
 import ec.edu.ups.hilos.Corredor;
+
 /**
  *
  * @author Fernanda
  */
 public class Principal {
+
     public static void main(String[] args) {
-        
         Corredor corredor = new Corredor();
-        
         corredor.Matriz();
-        
-        
     }
-    
-    
-    
-    
+/*
+        Corredor corredor1 = new Corredor("matriz1", 0);
+        Corredor corredor2 = new Corredor("matriz2", 0);
+        Corredor corredor3 = new Corredor("matriz3", 0);
+        Corredor corredor4 = new Corredor("matriz4", 0);
+        System.out.println("Inicia carrera");
+        
+        Thread hilo1 = new Thread(corredor1);
+        Thread hilo2 = new Thread(corredor2);
+        Thread hilo3 = new Thread(corredor3);
+        Thread hilo4 = new Thread(corredor4);
+        
+        hilo1.setPriority(2);
+        
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
+        
+        System.out.println("Finaliza carrera");
+
+    }
+*/
 }
